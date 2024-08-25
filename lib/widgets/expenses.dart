40893b5f259240f16e6471b21ecbdb8d73a5f73c
kotokotodonabe <ksk.pleiades@gmail.com>
2024-08-25 17:43:57 +0900

    feature: 費用を入力するためのモーダルの作成

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -26,6 +26,13 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('Modal bottom sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +40,7 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Flutter Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ],
