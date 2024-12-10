10322bf8684f71cfb0d8e2d6dc1d877223eac273
kotokotodonabe <ksk.pleiades@gmail.com>
2024-12-11 00:21:43 +0900

    feat: 経費のChartBarを作成

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -34,13 +34,13 @@ class Expense {
   }
 }
 
-class ExpensesBucket {
-  const ExpensesBucket({
+class ExpenseBucket {
+  const ExpenseBucket({
     required this.category,
     required this.expenses,
   });
 
-  ExpensesBucket.forCategory(List<Expense> allExpenses, this.category)
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
       : expenses = allExpenses
             .where((expense) => expense.category == category)
             .toList();
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -93,7 +94,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
